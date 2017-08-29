45e767c7fa923bac3ada75bcfea999c18376eadb
zhongfeixu <979345793@qq.com>
2017-08-29 23:57:40 +0800

    加了pulldown

---
+++ src/components/common/pulldown.vue
@@ -0,0 +1,48 @@
+<template>
+  <swiper :options="swiperOption">
+    <swiper-slide class="text">
+      <slot></slot>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script>
+  import {
+    swiper,
+    swiperSlide
+  } from 'vue-awesome-swiper'
+  export default {
+    data() {
+      let _this=this;
+      return {
+        swiperOption: {
+          direction: 'vertical',
+          slidesPerView: 'auto',
+          mousewheelControl: true,
+          freeMode: true,
+          onTouchEnd(swiper){
+               if(swiper.translate>200){
+                //  console.log(this)
+                   _this.$emit('pulldown');
+               }
+          }
+        }
+      }
+    },
+    methods:{
+      getNewData(){
+        alert(33333)
+      }  
+    },
+    components: {
+      swiper,
+      swiperSlide
+    }
+  }
+</script>
+
+<style >
+.swiper-slide.text {
+  height:auto;
+}
+</style>
